2059d67ce2723ddb21a5aa4ac475a7c95671de5a
unknown <anr.alexandre@gmail.com>
2020-11-09 21:57:44 -0300

    .

--- src/repositories/autocom-repository.js
+++ src/repositories/autocom-repository.js
@@ -2,13 +2,19 @@
 
 const connection = require('../mysql/mysql-connection');
 
-var deactivated = 
-	process.env.NODE_ENV === 'development' 
-		? require('../../.env.json').deactivated 
-		: process.env.deactivated;
+var deactivated;
+if (process.env.NODE_ENV === 'development') {
+	deactivated = require('../../.env.json').deactivated;
+} else {
+	deactivated = process.env.deactivated;
+};
 
 exports.getAutocomCnpj = (req, res) => {
-	if ( deactivated = "true" ) return res.status(400).send({ message: "Desativado" });
+	console.log("deactivated", deactivated);
+
+	if (deactivated == "true") {
+		return res.status(400).send({ message: "Desativado" })
+	};
 
 	getPorCnpj();
 
@@ -27,11 +33,11 @@ exports.getAutocomCnpj = (req, res) => {
 };
 
 exports.postAutocom = (req, res) => {
-	if ( deactivated = "true" ) return res.status(400).send({ message: "Desativado" });
-	if ( !validate() ) return res.status(400).send({ message: "Campos Obrigatorios não preenchidos" });
+	if (deactivated = "true") return res.status(400).send({ message: "Desativado" });
+	if (!validate()) return res.status(400).send({ message: "Campos Obrigatorios não preenchidos" });
 
 	postAutocomData();
-	
+
 	function validate() {
 		if (!req.body.Cnpj) return false;
 		if (!req.body.Produto) return false;
@@ -40,35 +46,35 @@ exports.postAutocom = (req, res) => {
 	function postAutocomData() {
 		verificaStatusCliente(req, (operationCode) => {
 			if (process.env.NODE_ENV === 'development') console.log(`POST_AUTOCOM.VERIFICA_STATUS_CLIENTE.operationCode : "${operationCode}"`);
-	
+
 			let interval = 0;
 			if (operationCode) {
 				interval = parseInt(operationCode) * 1000;
 				if (process.env.NODE_ENV === 'development') console.log("POST_AUTOCOM.VERIFICA_STATUS_CLIENTE.interval : ", interval);
 			};
-	
+
 			if (interval == 0) {
 				InsertAutocom(req, res);
 			} else {
 				setTimeout(function () { InsertAutocom(req, res); }, interval);
 			}
 		});
-		
+
 		function verificaStatusCliente(req, callback) {
 			var sql = 'select Cnpj, OperationCode, Produto from apiAutocom where (Cnpj = ?) and (Produto = ?)';
-	
+
 			if (process.env.NODE_ENV === 'development') console.log("VERIFICA_STATUS_CLIENTE.sql : ", sql);
-	
+
 			connection.query(sql, [req.body.Cnpj, req.body.Produto], function (err, rows, fields) {
 				if (err) {
 					console.log(">>>>>>>> VERIFICA_STATUS_CLIENTE.ERR", err);
 					return callback('');
 				};
-	
+
 				if (process.env.NODE_ENV === 'development') console.log("VERIFICA_STATUS_CLIENTE.rows : ", rows);
-	
+
 				let operationCode = '';
-				if ( !!rows.length ) {
+				if (!!rows.length) {
 					if (process.env.NODE_ENV === 'development') console.log("VERIFICA_STATUS_CLIENTE.rows.true");
 					try {
 						operationCode = rows[0].OperationCode;
@@ -79,16 +85,16 @@ exports.postAutocom = (req, res) => {
 				return callback(operationCode);
 			});
 		};
-	
+
 		function InsertAutocom(req, res) {
 			var dados = req.body;
-	
+
 			let data = new Date;
 			let dia = data.getDate();
 			let mes = data.getMonth() + 1;
 			let ano = data.getFullYear();
 			data = ano + '/' + mes + '/' + dia;
-	
+
 			let sVersaoAutocom = dados.VersaoAutocom;
 			if (sVersaoAutocom == 'undefined') {
 				sVersaoAutocom = '';
@@ -97,7 +103,7 @@ exports.postAutocom = (req, res) => {
 			if (sSatAtivacao == 'undefined') {
 				sSatAtivacao = '';
 			}
-	
+
 			let sql =
 				'INSERT INTO apiAutocom ' +
 				'( ' +
@@ -111,17 +117,17 @@ exports.postAutocom = (req, res) => {
 				'"' + dados.Produto + '", ' +
 				'"' + dados.Versao + '", ' +
 				'"' + data + '", ' +
-	
+
 				'"' + sVersaoAutocom + '", ' +
 				'"' + sSatAtivacao + '", ' +
 				'"' + dados.RazaoDadosCadastrais + '", ' +
 				'"' + dados.TelefoneDC + '", ' +
-	
+
 				'"' + dados.ContatoDC + '", ' +
 				'"' + dados.EnderecoDC + '", ' +
 				'"' + dados.NumeroDC + '", ' +
 				'"' + dados.CidadeDC + '", ' +
-	
+
 				'"' + dados.BairroDC + '", ' +
 				'"' + dados.EstadoDC + '", ' +
 				'"' + dados.CepDC + '"  ' +
@@ -132,19 +138,19 @@ exports.postAutocom = (req, res) => {
 				'Data = "' + data + '", ' +
 				'VersaoAutocom = "' + sVersaoAutocom + '", ' +
 				'SatAtivacao = "' + sSatAtivacao + '", ' +
-	
+
 				'RazaoDadosCadastrais = "' + dados.RazaoDadosCadastrais + '", ' +
 				'TelefoneDC = "' + dados.TelefoneDC + '", ' +
 				'ContatoDC = "' + dados.ContatoDC + '", ' +
 				'EnderecoDC = "' + dados.EnderecoDC + '", ' +
-	
+
 				'NumeroDC = "' + dados.NumeroDC + '", ' +
 				'CidadeDC = "' + dados.CidadeDC + '", ' +
 				'BairroDC = "' + dados.BairroDC + '", ' +
 				'EstadoDC = "' + dados.EstadoDC + '", ' +
-	
+
 				'CepDC = "' + dados.CepDC + '" ';
-	
+
 			connection.query(sql, [],
 				function (err, rows, fields) {
 					if (err) {
